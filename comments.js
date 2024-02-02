9ccc59a676e5fe154746fab52466c1cf22e85b5a
avilash b <91630253+AvilashBhowmick12@users.noreply.github.com>
2024-02-02 19:16:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,23 @@
-// Create webserver 
+//Create web Server
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const bodyParser = require('body-parser');
 
-var express = require('express');
-var router = express.Router();
-var Comment = require('../models/comment');
-var Post = require('../models/post');
-var User = require('../models/user');
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Create a comment
-router.post('/', function(req, res) {
-    var comment = new Comment({
-        content: req.body.content,})})
+//Set view engine to ejs
+app.set('view engine', 'ejs');
+
+//Set static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Set routes
+app.use(require('./routes/index'));
+app.use(require('./routes/comments'));
+app.use(require('./routes/users'));
+
+//Start server
+app.listen(port, () => console.log(`Server stated on port ${port}`));
